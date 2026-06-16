824ca4c2698edf7f2377299dddb53fbe069a1c32
alonsocanadilla1 <alonsocanadilla@gmail.com>
2026-06-16 16:25:39 +0200

    Add files via upload

---
+++ hardened/fase1/network.tf
@@ -0,0 +1,97 @@
+data "http" "deployer_ip" {
+  url = "https://ifconfig.me/ip"
+}
+
+resource "azurerm_virtual_network" "tfg_vnet" {
+  name                = "vnet-tfg"
+  location            = azurerm_resource_group.tfg_rg.location
+  resource_group_name = azurerm_resource_group.tfg_rg.name
+  address_space       = [ "10.0.0.0/16" ]
+}
+
+resource "azurerm_subnet" "management_subnet" {
+  name                 = "snet-management"
+  resource_group_name  = azurerm_resource_group.tfg_rg.name
+  virtual_network_name = azurerm_virtual_network.tfg_vnet.name
+  address_prefixes     = [ "10.0.1.0/24" ]
+  service_endpoints    = [ "Microsoft.Storage" ]
+}
+
+resource "azurerm_subnet" "dmz_subnet" {
+  name                 = "snet-dmz"
+  resource_group_name  = azurerm_resource_group.tfg_rg.name
+  virtual_network_name = azurerm_virtual_network.tfg_vnet.name
+  address_prefixes     = [ "10.0.2.0/24" ]
+  service_endpoints    = [ "Microsoft.Storage" ]
+}
+
+resource "azurerm_network_security_group" "management_nsg" {
+  name                = "nsg-management"
+  location            = azurerm_resource_group.tfg_rg.location
+  resource_group_name = azurerm_resource_group.tfg_rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = [ chomp(data.http.deployer_ip.response_body), "10.0.0.0/16" ]
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Caldera-Web"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8888"
+    source_address_prefixes    = [ chomp(data.http.deployer_ip.response_body), "10.0.0.0/16" ]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "dmz_nsg" {
+  name                = "nsg-dmz"
+  location            = azurerm_resource_group.tfg_rg.location
+  resource_group_name = azurerm_resource_group.tfg_rg.name
+
+  security_rule {
+    name                       = "Allow-HTTP-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefixes    = [ chomp(data.http.deployer_ip.response_body), azurerm_public_ip.c2_public_ip.ip_address, "10.0.0.0/16" ]
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH-From-Management"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = [ chomp(data.http.deployer_ip.response_body), "10.0.1.0/24" ]
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "management_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.management_subnet.id
+  network_security_group_id = azurerm_network_security_group.management_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "dmz_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.dmz_subnet.id
+  network_security_group_id = azurerm_network_security_group.dmz_nsg.id
+}
---
+++ hardened/fase1/provider.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azuread" {}
+
+resource "azurerm_resource_group" "tfg_rg" {
+  name     = "rg-tfg"
+  location = "spaincentral"
+}
